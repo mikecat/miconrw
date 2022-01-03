d64ed485e2ee348f01f94203659feaf33c840e37
MikeCAT <mikecat@users.noreply.github.com>
2022-01-03 21:56:04 +0900

    gdrw: add erase command

--- gdrw.c
+++ gdrw.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include <stdarg.h>
 #include <errno.h>
 #include "serial.h"
@@ -78,6 +79,92 @@ int readCommand(SerialPort* port, int argc, char* argv[]) {
 	return 0;
 }
 
+int eraseCommand(SerialPort* port, int argc, char* argv[]) {
+	char pagesToErase[256] = {0};
+	unsigned char sendBuffer[256 + 2], recvBuffer;
+	int isGlobal = 0;
+	int i;
+	int erasePageCount;
+	for (i = 0; i < argc; i++) {
+		if (strcmp(argv[i], "global") == 0) {
+			isGlobal = 1;
+			break;
+		} else {
+			/* DFA that accepts strings like "123" or "12-34" */
+			int status = 0;
+			int j;
+			for (j = 0; argv[i][j] != '\0'; j++) {
+				switch (status) {
+					case 0:
+						if (isdigit((unsigned char)argv[i][j])) status = 1;
+						else status = 4;
+						break;
+					case 1:
+						if (isdigit((unsigned char)argv[i][j])) status = 1;
+						else if (argv[i][j] == '-') status = 2;
+						else status = 4;
+						break;
+					case 2:
+						if (isdigit((unsigned char)argv[i][j])) status = 3;
+						else status = 4;
+						break;
+					case 3:
+						if (isdigit((unsigned char)argv[i][j])) status = 3;
+						else status = 4;
+						break;
+				}
+			}
+			if (status == 1) {
+				char* end;
+				unsigned long no;
+				errno = 0;
+				no = strtoul(argv[i], &end, 10);
+				if (errno == ERANGE || no > 0xff) return errorRet(1, "invalid page number\n");
+				pagesToErase[no] = 1;
+			} else if (status == 3) {
+				char* end;
+				unsigned long noStart, noEnd, no;
+				errno = 0;
+				noStart = strtoul(argv[i], &end, 10);
+				if (errno == ERANGE) return errorRet(1, "invalid page range\n");
+				noEnd = strtoul(end + 1, &end, 10);
+				if (errno == ERANGE || noEnd < noStart || noEnd > 0xff) return errorRet(1, "invalid page range\n");
+				for (no = noStart; no <= noEnd; no++) pagesToErase[no] = 1;
+			} else {
+				return errorRet(1, "invalid argument: %s\n", argv[i]);
+			}
+		}
+	}
+	erasePageCount = 0;
+	for (i = 0; i < 256; i++) {
+		if (pagesToErase[i]) {
+			sendBuffer[++erasePageCount] = (unsigned char)i;
+		}
+	}
+	if (!isGlobal && erasePageCount == 0) {
+		return errorRet(1, "no page to erase\n");
+	}
+	if (serialSend(port, "\x43\xbc", 2, 1) != 2) return errorRet(1, "failed to send\n");
+	if (serialRecv(port, &recvBuffer, 1, 1) != 1) return errorRet(1, "failed to receive\n");
+	if (recvBuffer != ACK) return errorRet(1, "what is returned is not ACK\n");
+	if (isGlobal) {
+		sendBuffer[0] = 0xff;
+		sendBuffer[1] = 0x00;
+		if (serialSend(port, sendBuffer, 2, 1) != 2) return errorRet(1, "failed to send\n");
+	} else {
+		unsigned char checksum = 0;
+		sendBuffer[0] = (unsigned char)(erasePageCount - 1);
+		for (i = 0; i <= erasePageCount; i++) {
+			checksum ^= sendBuffer[i];
+		}
+		sendBuffer[erasePageCount + 1] = checksum;
+		if (serialSend(port, sendBuffer, erasePageCount + 2, 1) != erasePageCount + 2) return errorRet(1, "failed to send\n");
+	}
+	if (serialRecv(port, &recvBuffer, 1, 1) != 1) return errorRet(1, "failed to receive\n");
+	if (recvBuffer != ACK) return errorRet(1, "what is returned is not ACK\n");
+	return 0;
+}
+
 int main(int argc, char* argv[]) {
 	SerialPort* port;
 	int ret = 0;
@@ -86,6 +173,8 @@ int main(int argc, char* argv[]) {
 		fprintf(stderr, "command:\n");
 		fprintf(stderr, "  init\n");
 		fprintf(stderr, "  read <start_address> <length> <out_file>\n");
+		fprintf(stderr, "  erase global\n");
+		fprintf(stderr, "  erase <page number> [<page number> ...]\n");
 		return 1;
 	}
 	port = serialInit(argv[1], 57600, 0, 0, PALITY_EVEN);
@@ -97,6 +186,8 @@ int main(int argc, char* argv[]) {
 		ret = initCommand(port, argc - 3, argv + 3);
 	} else if (strcmp(argv[2], "read") == 0) {
 		ret = readCommand(port, argc - 3, argv + 3);
+	} else if (strcmp(argv[2], "erase") == 0) {
+		ret = eraseCommand(port, argc - 3, argv + 3);
 	} else {
 		fprintf(stderr, "unknown command\n");
 		ret = 1;
